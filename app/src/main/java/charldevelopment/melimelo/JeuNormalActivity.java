97dddfb172d897514bdbadf6267fb39e5b9d1c15
Unknown <charlie.leger@etu.unilim.fr>
2019-03-07 08:54:47 +0100

    ajout activiteJeu

---
+++ app/src/main/java/charldevelopment/melimelo/JeuNormalActivity.java
@@ -0,0 +1,13 @@
+package charldevelopment.melimelo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JeuNormalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jeu_normal);
+    }
+}
