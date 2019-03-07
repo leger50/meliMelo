ceac8f71e2ebbec0d229d23e565c36512fc307fe
Nathan Liboutet <nathan.lib19@gmail.com>
2019-03-07 18:01:05 +0100

    accueil + début paramètre partie

--- app/src/main/java/charldevelopment/melimelo/activities/AccueilActivity.java
+++ app/src/main/java/charldevelopment/melimelo/activities/AccueilActivity.java
@@ -1,15 +1,27 @@
 package charldevelopment.melimelo.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import charldevelopment.melimelo.R;
 
-public class AccueilActivity extends AppCompatActivity {
+public class AccueilActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        Button jouer = (Button) findViewById(R.id.btn_play);
+        jouer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(AccueilActivity.this, ParametresPartieActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/charldevelopment/melimelo/activities/ParametresPartieActivity.java
@@ -0,0 +1,15 @@
+package charldevelopment.melimelo.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import charldevelopment.melimelo.R;
+
+public class ParametresPartieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parametres_partie);
+    }
+}
