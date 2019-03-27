0aca5b96913b9da0ef2c97af55a1cb10dbbe4895
Quentin <quentin.lenoir@etu.unilim.fr>
2019-03-27 16:51:06 +0100

    séparation dans la classe

--- app/src/main/java/charldevelopment/melimelo/activities/JeuNormalActivity.java
+++ app/src/main/java/charldevelopment/melimelo/activities/JeuNormalActivity.java
@@ -13,6 +13,7 @@ import java.lang.reflect.Array;
 import java.util.Random;
 
 import charldevelopment.melimelo.R;
+import charldevelopment.melimelo.classes.GrilleJeu;
 import charldevelopment.melimelo.database.models.Mot;
 import charldevelopment.melimelo.database.repositories.MotDataRepository;
 import charldevelopment.melimelo.views.MotViewModel;
@@ -20,9 +21,7 @@ import charldevelopment.melimelo.views.MotViewModel;
 public class JeuNormalActivity extends AppCompatActivity {
 
     private List<Mot> listeMots;
-
-    private final int TAILLE = 10;
-    private String[][] grille;
+    private GrilleJeu grille;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,64 +35,7 @@ public class JeuNormalActivity extends AppCompatActivity {
         this.listeMots = motView.obtenirListeMots().getValue();
         System.out.println(this.listeMots.toString());
         System.out.println("Size : " + this.listeMots.size());*/
+        this.grille = new GrilleJeu();
 
-
-        this.grille = new String[this.TAILLE][this.TAILLE];
-
-        for (int i = 0; i < this.TAILLE;i++){
-            for (int j = 0; j < this.TAILLE; j++){
-                this.grille[i][j] = "." ;
-            }
-        }
-
-        this.insererMot("coucou");
-        this.insererMot("Charlie");
-        this.insererMot("Nathan");
-        this.insererMot("Quentin");
-        this.insererMot("Lara");
-        this.insererMot("Pokemon");
-
-        this.toString();
-    }
-
-    public String toString(){
-
-        String retour = "";
-
-        for (int i = 0; i < this.TAILLE;i++){
-            for (int j = 0; j < this.TAILLE; j++){
-               retour += this.grille[i][j];
-            }
-            System.out.println(retour);
-            retour = "";
-
-        }
-
-        return retour;
-    }
-
-    public void insererMot (String mot){
-        Random random = new Random();
-
-        String[] tabMot = mot.split("");
-
-        int ligne = random.nextInt(this.TAILLE);
-
-        int colonneMax = this.TAILLE-mot.length();
-
-        for(int k = 0; k < colonneMax; k++){
-            if(!this.grille[ligne][k].equals(".")){
-                ligne = random.nextInt(this.TAILLE);
-                k=0;
-            }
-        }
-
-        int colonne = random.nextInt(colonneMax);
-
-        int index = 1;
-        for(int i = colonne; i < mot.length()+colonne; i++){
-            this.grille[ligne][i]=tabMot[index];
-            index++;
-        }
     }
 }
--- app/src/main/java/charldevelopment/melimelo/classes/GrilleJeu.java
+++ app/src/main/java/charldevelopment/melimelo/classes/GrilleJeu.java
@@ -1,29 +1,68 @@
 package charldevelopment.melimelo.classes;
 
+import java.util.Random;
+
 public class GrilleJeu {
 
-    private char[][] grilleLettres;
-    private int tailleGrilleHorizontale;
-    private int tailleGrilleVerticale;
+    private final int TAILLE = 10;
+    private String[][] grille;
 
-    public GrilleJeu(int tailleGrilleHorizontale, int tailleGrilleVerticale){
-        this.grilleLettres = new char[this.tailleGrilleHorizontale][this.tailleGrilleVerticale];
-        this.tailleGrilleHorizontale = tailleGrilleHorizontale;
-        this.tailleGrilleVerticale = tailleGrilleVerticale;
+    public GrilleJeu() {
+        this.grille = new String[this.TAILLE][this.TAILLE];
+        for (int i = 0; i < this.TAILLE;i++) {
+            for (int j = 0; j < this.TAILLE; j++) {
+                this.grille[i][j] = ".";
+            }
 
-        this.initialiserGrille();
+        }
+        this.insererMot("COUCOU");
+        this.insererMot("CHARLIE");
+        this.insererMot("NATHAN");
+        this.insererMot("QUENTIN");
+        this.insererMot("LARA");
+        this.insererMot("POKEMON");
+
+        this.toString();
     }
 
-    private void initialiserGrille(){
+    public String toString(){
 
-        char lettre = 'a';
+        String retour = "";
 
-        for(int i=0; i<this.tailleGrilleHorizontale; i++){
-            for (int j=0; j<this.tailleGrilleVerticale; j++){
-                this.grilleLettres[i][j] = lettre;
-                lettre++;
+        for (int i = 0; i < this.TAILLE;i++){
+            for (int j = 0; j < this.TAILLE; j++){
+                retour += this.grille[i][j];
+            }
+            System.out.println(retour);
+            retour = "";
+
+        }
+
+        return retour;
+    }
+
+    public void insererMot (String mot){
+        Random random = new Random();
+
+        String[] tabMot = mot.split("");
+
+        int ligne = random.nextInt(this.TAILLE);
+
+        int colonneMax = this.TAILLE-mot.length();
+
+        for(int k = 0; k < colonneMax; k++){
+            if(!this.grille[ligne][k].equals(".")){
+                ligne = random.nextInt(this.TAILLE);
+                k=0;
             }
         }
 
+        int colonne = random.nextInt(colonneMax);
+
+        int index = 1;
+        for(int i = colonne; i < mot.length()+colonne; i++){
+            this.grille[ligne][i]=tabMot[index];
+            index++;
+        }
     }
 }
