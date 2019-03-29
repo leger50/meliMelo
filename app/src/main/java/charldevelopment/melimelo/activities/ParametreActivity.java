5dc5e1a2843307be6a3baba8b882bb82ae70fe58
Unknown <charlie.leger@etu.unilim.fr>
2019-03-29 16:56:21 +0100

    Seule une langue peut être selectionnée

--- app/src/main/java/charldevelopment/melimelo/activities/ParametreActivity.java
+++ app/src/main/java/charldevelopment/melimelo/activities/ParametreActivity.java
@@ -5,13 +5,18 @@ import android.content.SharedPreferences;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import charldevelopment.melimelo.R;
 
 public class ParametreActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Switch switch_langueFrancais;
+    private Switch switch_langueAnglais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +31,19 @@ public class ParametreActivity extends AppCompatActivity implements View.OnClick
         boolean vibreur = prefs.getBoolean("vibreur",true);//"No name defined" is the default value
         boolean notifications = prefs.getBoolean("notifications",true);
 
-
         switch_vibreur.setChecked(vibreur);
         switch_notifications.setChecked(notifications);
 
         retour.setOnClickListener(this);
         switch_notifications.setOnClickListener(this);
         switch_vibreur.setOnClickListener(this);
+
+        //Paramètre des langues
+        this.switch_langueFrancais = (Switch) findViewById(R.id.switch_french);
+        this.switch_langueAnglais = (Switch) findViewById(R.id.switch_english);
+
+        this.switch_langueFrancais.setOnCheckedChangeListener(new OnCheckedChange_SwitchFrancais());
+        this.switch_langueAnglais.setOnCheckedChangeListener(new OnCheckedChange_SwitchAnglais());
     }
 
     @Override
@@ -63,5 +74,29 @@ public class ParametreActivity extends AppCompatActivity implements View.OnClick
                 break;
         }
 
+    }
+
+    private class OnCheckedChange_SwitchFrancais implements CompoundButton.OnCheckedChangeListener{
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if(isChecked)
+                switch_langueAnglais.setChecked(false);
+
+            else
+                switch_langueAnglais.setChecked(true);
+        }
+    }
+
+    private class OnCheckedChange_SwitchAnglais implements CompoundButton.OnCheckedChangeListener{
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if(isChecked)
+                switch_langueFrancais.setChecked(false);
+
+            else
+                switch_langueFrancais.setChecked(true);
+        }
     }
 }
